9ffe8b1b90057a1df941678e9365e7ef34afdf10
landys <landys.cn@gmail.com>
2009-06-02 15:06:40 +0000

    Modify ResizeImages codes.

--- ResizeImages/main.cpp
+++ ResizeImages/main.cpp
@@ -1,20 +1,24 @@
 #include <Magick++.h>
 #include <iostream>
+#include <string>
+#include <cstdio>
+#include <cstring>
 using namespace Magick;
 using namespace std;
 
 #pragma comment(lib, "CORE_RL_Magick++_.lib")
 
 // the imgsfile should utf-8 encoded.
-void resize(const char* imgsfile)
+void resize(const char* imgsfile, const char* refileNmae, const char* inputPart, const char* outputPart)
 {
 	
 	int n = 0, i, j;
 	FILE * imageset = fopen(imgsfile, "rt");
-	FILE * refile = fopen("E:\\pic_skindetect\\clothtest\\zz\\zz3.txt", "wt");
+	FILE * refile = fopen(refileNmae, "wt");
 	char imagename[255];
-	char reimg[255];
+	//char reimg[255];
 	int re = 0;
+	int nin = strlen(inputPart);
 
 	while(fgets(imagename, 255, imageset) != NULL)
 	{
@@ -22,17 +26,24 @@ void resize(const char* imgsfile)
 		for (i=0; imagename[i] != '\n' && imagename[i] != '\r' && imagename[i] != '\0'; ++i); // loop stop here
 		imagename[i] = '\0';
 		//fprintf(refile, "%s\n", imagename);
+
+		string reimg(imagename);
+		j = reimg.find(inputPart);
+		if (j == -1)
+		{
+			fprintf(refile, "wrong generate result file name: %s\n", imagename);
+			continue;
+		}
+		reimg.replace(j, nin, outputPart);
+
 		Image img;
 		try {
 			img.read(imagename);
 			img.resize(Geometry(200, 200));
-			strncpy(reimg, imagename, 28);
-			strcpy(reimg+28, "zz200");
-			strcpy(reimg+33, imagename+30);
-			img.write(reimg);
+			img.write(reimg.c_str());
 			re++;
 		} catch( ... ) {
-			fprintf(refile, "wrong reading %s\n", imagename);
+			fprintf(refile, "wrong reading: %s\n", imagename);
 		}
 
 		
@@ -46,5 +57,14 @@ void resize(const char* imgsfile)
 
 int main(int argc, char** argv)
 {
-	resize("E:\\pic_skindetect\\clothtest\\zz\\zz.txt");
+	if (argc < 5)
+	{
+		printf("Usage %s input_image_list_file output_log_file input_path_part diff_of_output_path_part\n", argv[0]);
+		return 1;
+	}
+
+	resize(argv[1], argv[2], argv[3], argv[4]);
+
+	return 0;
 }
+
