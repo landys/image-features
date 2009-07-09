1fd48b1af19ebd4be11491b3621d59f16b0972b7
landys <landys.cn@gmail.com>
2009-07-09 03:15:00 +0000

    Commit ImageMatcher/Fourier.h and ResizeImages/main.cpp.

--- ImageMatcher/Fourier.h
+++ ImageMatcher/Fourier.h
@@ -1,4 +1,8 @@
 #pragma once
 
-// n*2 is dimension
+/**
+ * n dimension
+ * feature its size must be n*2
+ * return 1-success, 0-fail
+ */
 int extractFourier(const char* imagenamefile, int n, double* feature);
--- ResizeImages/main.cpp
+++ ResizeImages/main.cpp
@@ -8,8 +8,14 @@ using namespace std;
 
 #pragma comment(lib, "CORE_RL_Magick++_.lib")
 
-// the imgsfile should utf-8 encoded.
-void resize(const char* imgsfile, const char* refileNmae, const char* inputPart, const char* outputPart)
+/** the imgsfile should utf-8 encoded. Default of max width and height are both 512, 
+ * which means the size of its larger dimension is 512 pixels.
+ */
+void resize(const char* imgsfile, const char* refileNmae, const char* inputPart, const char* outputPart, int width=512, int height=512);
+
+// the imgsfile should utf-8 encoded. Default of max width and height are both 512, 
+// which means the size of its larger dimension is 512 pixels.
+void resize(const char* imgsfile, const char* refileNmae, const char* inputPart, const char* outputPart, int width, int height)
 {
 	
 	int n = 0, i, j;
@@ -17,11 +23,17 @@ void resize(const char* imgsfile, const char* refileNmae, const char* inputPart,
 	FILE * refile = fopen(refileNmae, "wt");
 	char imagename[255];
 	//char reimg[255];
-	int re = 0;
+	long long re = 0;
 	int nin = strlen(inputPart);
 
+	long long count = 0;
 	while(fgets(imagename, 255, imageset) != NULL)
 	{
+		if (++count % 1000 == 0)
+		{
+			printf("current processed: %lld\n", count);
+		}
+
 		int i = 0;
 		for (i=0; imagename[i] != '\n' && imagename[i] != '\r' && imagename[i] != '\0'; ++i); // loop stop here
 		imagename[i] = '\0';
@@ -39,31 +51,50 @@ void resize(const char* imgsfile, const char* refileNmae, const char* inputPart,
 		Image img;
 		try {
 			img.read(imagename);
-			img.resize(Geometry(200, 200));
+			if (img.columns() < width && img.rows() < height)
+			{
+				continue;
+			}
+			img.resize(Geometry(width, height));
 			img.write(reimg.c_str());
 			re++;
 		} catch( ... ) {
 			fprintf(refile, "wrong reading: %s\n", imagename);
 		}
-
-		
 	}
 
-	printf("%d images finished.\n", re);
+	printf("%lld images processed.\n", count);
+	printf("%lld images finished.\n", re);
 	fclose(imageset);
 	fclose(refile);
 }
 
+/*void dotest()
+{
+	Image img;
+	img.read("J:\\pic_bak\\cgfa.sunsite.dk\\a\\t-alexander2.jpg");
+	printf("%d, %d\n", img.columns(), img.rows());
+}*/
 
+// NOTICE: the image list file should be in UTF-8 encoding, if some chinese characters!
 int main(int argc, char** argv)
 {
 	if (argc < 5)
 	{
-		printf("Usage %s input_image_list_file output_log_file input_path_part diff_of_output_path_part\n", argv[0]);
+		printf("Usage %s input_image_list_file output_log_file input_path_part diff_of_output_path_part max_width max_height\n", argv[0]);
 		return 1;
 	}
 
-	resize(argv[1], argv[2], argv[3], argv[4]);
+	int width = 512;
+	int height = 512;
+
+	if (argc >= 7)
+	{
+		sscanf(argv[5], "%d", &width);
+		sscanf(argv[6], "%d", &height);
+	}
+
+	resize(argv[1], argv[2], argv[3], argv[4], width, height);
 
 	return 0;
 }
